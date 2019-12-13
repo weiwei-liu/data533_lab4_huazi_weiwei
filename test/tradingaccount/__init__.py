04afc2a50a1346a7c421ffdc1797335a8cf543ea
Weiwei Liu <weiwei-liu>
2019-12-12 19:44:36 -0800

    update

--- test/tradingaccount/__init__.py
+++
@@ -1,1 +0,0 @@
-'''Trading Account is a python package that allows users to track their transaction records and generate visualization for both balance and stock history. Candlestick may be applied as the primary drawing method.'''
--- test/tradingaccount/account/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 29 14:34:19 2019
-
-@author: weiweiliu
-"""
-
-__all__=['account','cnaccount','usaccount']
